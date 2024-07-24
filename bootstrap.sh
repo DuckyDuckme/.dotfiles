00c721e3fbfad867ea28a01e424f78d93f6787db
Krzysztof Jan Pudowski <krzysiek.pudowski@gmail.com>
2024-07-25 01:53:18 +0200

    moved nvim vim-plug configuration to scripts/user_setup instead

--- bootstrap.sh
+++ bootstrap.sh
@@ -27,11 +27,6 @@ install() {
     if [ ! -d "$HOME/.powerlevel10k" ]; then
 	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.powerlevel10k
     fi
-
-    if [ ! -f "${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload/plug.vim" ]; then
-    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-    fi
 }
 
 print_usage() {
